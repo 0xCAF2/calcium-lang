21814065fa0b915f5c55b1a26e8acd631108af3e
0xCAF2 <ht@0xcaf2.dev>
2021-02-23 05:44:26 +0900

    rename files

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import Engine from './engine';
-import * as Index from './index';
-import * as Keyword from './keyword';
+import * as Index from './indexes';
+import * as Keyword from './keywords';
 import Status from './status';
 
 const Calcium = {
