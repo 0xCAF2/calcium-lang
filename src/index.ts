396a38610bf8391fe4a5a1d81a94d0204cdccad2
0xCAF2 <pub@0xcaf2.dev>
2022-02-04 17:33:58 +0900

    add export members

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { default as Runtime } from "./runtime/runtime";
 export { default as Status } from "./runtime/status";
+export { default as Index } from "./indexes";
+export * as Keyword from "./keyword";
