9309b21ffa3d6cf92b93b1c5061d1130cfac606a
0xCAF2 <pub@0xcaf2.dev>
2022-04-03 15:08:46 +0900

    rename component

--- src/components/commands/EndCmd.vue
+++ src/components/commands/EndCmd.vue
@@ -1,13 +1,13 @@
 <template>
-  <insert-above />
+  <add-new-cmd />
   <p>プログラムの終わり</p>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
-import InsertAbove from "../InsertAbove.vue";
+import AddNewCmd from "../AddNewCmd.vue";
 
 export default defineComponent({
   name: "EndCmd",
-  components: { InsertAbove },
+  components: { AddNewCmd },
 });
 </script>
--- src/components/commands/ExprStmt.vue
+++ src/components/commands/ExprStmt.vue
@@ -1,17 +1,17 @@
 <template>
-  <insert-above />
+  <add-new-cmd />
   <div>{{ expr }}</div>
 </template>
 <script lang="ts">
 import { defineComponent, type PropType } from "vue";
 import type { Expr } from "../../types/Expr";
-import InsertAbove from "../InsertAbove.vue";
+import AddNewCmd from "../AddNewCmd.vue";
 
 export default defineComponent({
   name: "ExprStmt",
   props: {
     expr: Object as PropType<Expr>,
   },
-  components: { InsertAbove },
+  components: { AddNewCmd },
 });
 </script>
